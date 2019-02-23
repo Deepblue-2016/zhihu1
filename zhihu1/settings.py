fa0b96f9be073d5d80bdebc4bc2e57b27d8a05bd
deepblue-2016 <sunxin@neusoft.com>
2019-02-23 15:14:43 +0800

    1.1

--- zhihu1/settings.py
+++ zhihu1/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'zhihu1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
@@ -100,9 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
